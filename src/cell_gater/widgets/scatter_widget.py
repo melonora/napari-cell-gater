059b7a4326a36902dad9fe2fee1198625a6cf608
josenimo <nimojose@gmail.com>
2024-04-10 17:38:57 +0200

    plot updates, yaxis title updates

--- src/cell_gater/widgets/scatter_widget.py
+++ src/cell_gater/widgets/scatter_widget.py
@@ -17,12 +17,12 @@ from qtpy.QtWidgets import (
     QLabel,
     QSizePolicy,
     QVBoxLayout,
+    QPushButton,
     QWidget,
     QGridLayout,
 )
 
 from cell_gater.model.data_model import DataModel
-
 
 class ScatterInputWidget(QWidget):
     """Widget for a scatter plot with markers on the x axis and any dtype column on the y axis."""
@@ -52,6 +52,9 @@ class ScatterInputWidget(QWidget):
         self.marker_selection_dropdown.addItems(self.model.markers)
         self.marker_selection_dropdown.currentTextChanged.connect(self._on_marker_changed)
 
+        apply_button = QPushButton("Load Sample and Marker")
+        apply_button.clicked.connect(self._load_images_and_scatter_plot)
+
         choose_y_axis_label = QLabel("Choose Y-axis")
         self.choose_y_axis_dropdown = QComboBox()
         self.choose_y_axis_dropdown.addItems(self.model.regionprops_df.columns)
@@ -62,9 +65,10 @@ class ScatterInputWidget(QWidget):
         self.layout().addWidget(self.sample_selection_dropdown, 1, 0)
         self.layout().addWidget(marker_label, 2, 0)
         self.layout().addWidget(self.marker_selection_dropdown, 3, 0)
-        self.layout().addWidget(choose_y_axis_label, 4, 0)
-        self.layout().addWidget(self.choose_y_axis_dropdown, 5, 0)
-        
+        self.layout().addWidget(apply_button, 4, 0)
+        self.layout().addWidget(choose_y_axis_label, 5, 0)
+        self.layout().addWidget(self.choose_y_axis_dropdown, 6, 0)
+
         # we have to do this because initially the dropdowns did not change texts yet so these variables are still None.
         self.model.active_sample = self.sample_selection_dropdown.currentText()
         self.model.active_marker = self.marker_selection_dropdown.currentText()
@@ -73,14 +77,15 @@ class ScatterInputWidget(QWidget):
         self._read_data(self.model.active_sample)
         self._load_layers(self.model.markers[self.model.active_marker])
 
-        #this has to go after active sample and marker are set
         self.scatter_canvas = PlotCanvas(self.model)
-        # self.layout().addWidget(NavigationToolbar(self.gate_canvas, self))
-        self.layout().addWidget(self.scatter_canvas.fig, 6, 0)
-        # Update the plot initially
+        self.layout().addWidget(self.scatter_canvas.fig, 7, 0)
         self.update_plot()
-        # the scatter plot is not updating when the gate is changed
-        # unsure what is happening here
+
+    def _load_images_and_scatter_plot(self):
+        self._clear_layers(clear_all=True)
+        self._read_data(self.model.active_sample)
+        self._load_layers(self.model.markers[self.model.active_marker])
+        self.update_plot()
 
     @property
     def model(self) -> DataModel:
@@ -107,16 +112,18 @@ class ScatterInputWidget(QWidget):
 
             self._image = imread(image_path)
             self._mask = imread(mask_path)
+            #TODO add DNA channel to the layers added
 
     def _load_layers(self, marker_index):
 
-        if self.model.active_sample != self._current_sample:
-            self._current_sample = copy(self.model.active_sample)
-            self.viewer.add_labels(
-                self._mask, 
-                name="mask_" + self.model.active_sample,
-                visible=False, opacity=0.4
-            )
+        # FOR NOW
+        # if self.model.active_sample != self._current_sample:
+        #     self._current_sample = copy(self.model.active_sample)
+        self.viewer.add_labels(
+            self._mask, 
+            name="mask_" + self.model.active_sample,
+            visible=False, opacity=0.4
+        )
 
         self.viewer.add_image(
             self._image[marker_index],
@@ -125,19 +132,9 @@ class ScatterInputWidget(QWidget):
         )
 
     def _on_sample_changed(self):
-        """Set the active sample.
-
-        This changes the active sample and clears the layers and the marker selection dropdown.
-        Subsequently, the new layers are loaded.
-        """
         self.model.active_sample = self.sample_selection_dropdown.currentText()
-
-        self._clear_layers(clear_all=True)
-        self._reinitiate_marker_selection_dropdown()
-
-        self._read_data(self.model.active_sample)
-        self._load_layers(self.model.markers[self.model.active_marker])
-        self.update_plot()
+    def _on_marker_changed(self):
+        self.model.active_marker = self.marker_selection_dropdown.currentText()
 
     def _clear_layers(self, clear_all: bool) -> None:
         """Remove all layers upon changing sample."""
@@ -149,21 +146,14 @@ class ScatterInputWidget(QWidget):
                 if isinstance(layer, Image):
                     self.viewer.layers.remove(layer)
 
-    def _reinitiate_marker_selection_dropdown(self) -> None:
-        """Reiniatiate the marker selection dropdown after sample has changed."""
-        # This is preemptively added for clearing visual completed feedback once implemented.
-        # We also block the outgoing signal in order not to update the layer when there is no current active marker.
-        self.marker_selection_dropdown.blockSignals(True)
-        self.marker_selection_dropdown.clear()
-        self.marker_selection_dropdown.addItems(self.model.markers)
-        self.marker_selection_dropdown.blockSignals(False)
-
-    def _on_marker_changed(self):
-        """Set active marker and update the marker image layer."""
-        self.model.active_marker = self.marker_selection_dropdown.currentText()
-        self._clear_layers(clear_all=False)
-        self._load_layers(self.model.markers[self.model.active_marker])
-        self.update_plot()
+    # def _reinitiate_marker_selection_dropdown(self) -> None:
+    #     """Reiniatiate the marker selection dropdown after sample has changed."""
+    #     # This is preemptively added for clearing visual completed feedback once implemented.
+    #     # We also block the outgoing signal in order not to update the layer when there is no current active marker.
+    #     self.marker_selection_dropdown.blockSignals(True)
+    #     self.marker_selection_dropdown.clear()
+    #     self.marker_selection_dropdown.addItems(self.model.markers)
+    #     self.marker_selection_dropdown.blockSignals(False)
 
     def _set_samples_dropdown(self) -> None:
         """Set the items for the samples dropdown QComboBox."""
@@ -182,10 +172,9 @@ class ScatterInputWidget(QWidget):
         self.update_plot()
 
     def update_plot(self):
-        #clear the plot
         self.scatter_canvas.ax.clear()
-        self.scatter_canvas.plot_scatter_plot()
-
+        self.scatter_canvas.plot_scatter_plot(self.model)
+        self.scatter_canvas.fig.draw()
 
 class PlotCanvas():
     """The canvas class for the gating scatter plot."""
@@ -220,6 +209,8 @@ class PlotCanvas():
         df = self.model.regionprops_df
         df = df[df["sample_id"] == self.model.active_sample]
 
+        # self.model.active_y_axis = "Area"
+
         self.ax.scatter(
             x=df[self.model.active_marker],
             y=df[self.model.active_y_axis],  # later change to desired_y_axis
@@ -230,12 +221,12 @@ class PlotCanvas():
             s=80000 / int(df.shape[0]),
         )
     #
-        self.ax.set_ylabel("Area")  # later change to desired_y_axis
+        self.ax.set_ylabel(self.model.active_y_axis)
         self.ax.set_xlabel(f"{self.model.active_marker} intensity")
     #
-    #     # add vertical line at current gate if it exists
-    #     if self.model.current_gate is not None:
-    #         ax.axvline(x=self.model.current_gate, color="red", linewidth=1.0, linestyle="--")
+        # add vertical line at current gate if it exists
+        if self.model.current_gate is not None:
+            self.ax.axvline(x=self.model.current_gate, color="red", linewidth=1.0, linestyle="--")
     #
     #     minimum = df[self.model.active_marker].min()
     #     maximum = df[self.model.active_marker].max()
